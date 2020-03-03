003debc5c7df22fb3001fa25461f032d871d48a8
jgstew <james@jgstew.com>
2020-03-03 15:02:41 -0500

    initial commit

---
+++ examples/task_example_data.py
@@ -0,0 +1,7 @@
+
+template_dict = {
+                'template': 'examples/TEMPLATE_TASK.bes',
+                'title': 'Example Generated From Template!',
+                'size': 7,
+                'prefetch': 'prefetch file.txt'
+                }
---
+++ generate-bes-from-template.py
@@ -0,0 +1,15 @@
+#!/usr/local/python
+"""
+generate-bes-from-template.py
+
+Created by James Stewart (@JGStew) on 2020-03-03.
+"""
+
+from examples.task_example_data import template_dict
+
+def main():
+    print(template_dict)
+
+# if called directly, then run this example:
+if __name__ == '__main__':
+    main()

a6361fdd6b5a8bd6a79f60dee925e2d808f46920
jgstew <james@jgstew.com>
2020-03-03 15:27:08 -0500

    add comments, example data to work with

--- generate_bes_from_template.py
+++ generate_bes_from_template.py
@@ -5,13 +5,25 @@ generate-bes-from-template.py
 Created by James Stewart (@JGStew) on 2020-03-03.
 """
 
-# get python dictionary with config items
-from examples.task_example_data import template_dict
+from __future__ import absolute_import
+
+def generate_bes_from_template(template_dict):
+    """Generate BES XML file from info in template_dict hash table"""
+    print(template_dict)
 
 
 def main():
     """Only called if this script is run directly"""
-    print(template_dict)
+    # get python dictionary with example config items (only works for python3?)
+    #from examples.task_example_data import template_dict
+    #print(template_dict)
+    template_dict = {
+                'template': 'examples/TEMPLATE_TASK.bes',
+                'title': 'Example Generated From Template!',
+                'size': 7,
+                'prefetch': 'prefetch file.txt'
+                }
+    generate_bes_from_template(template_dict)
 
 # if called directly, then run this example:
 if __name__ == '__main__':

fe337c625fbabbc0df5c53ec2510b48da06d1415
jgstew <james@jgstew.com>
2020-03-03 15:07:17 -0500

    add doc strings, new line at end of file

--- generate_bes_from_template.py
+++ generate_bes_from_template.py
@@ -8,7 +8,9 @@ Created by James Stewart (@JGStew) on 2020-03-03.
 # get python dictionary with config items
 from examples.task_example_data import template_dict
 
+
 def main():
+    """Only called if this script is run directly"""
     print(template_dict)
 
 # if called directly, then run this example:

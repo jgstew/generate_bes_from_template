83b7820e5ae57edce7cc860eb07b307d4d2c1180
jgstew <james@jgstew.com>
2020-03-11 15:41:50 -0400

    basics are working

--- action_createfile_from_file.py
+++ action_createfile_from_file.py
@@ -3,27 +3,44 @@
 action_createfile_from_file.py
 
 Created by James Stewart (@JGStew) on 2020-03-11.
+
+Related:
+  - https://bigfix.me/relevance/details/3022868
 """
 from __future__ import absolute_import
 
 #import os
 
-#import pystache
+import pystache
+
+pystache_template_createfile = """\
+delete __createfile
+
+createfile until _END_OF_FILE_
+{{{file_contents}}}
+_END_OF_FILE_
+
+delete "{{{file_path_destination}}}"
+copy __createfile "{{{file_path_destination}}}"
+"""
 
 def action_createfile_from_file(file_path, file_path_destination=None):
     """Read text file, turn into BigFix Action CreateFile Commnad"""
+    template_dict = {}
     if not file_path_destination:
-        file_path_destination = file_path
+        template_dict['file_path_destination'] = file_path
+    else:
+        template_dict['file_path_destination'] = file_path_destination
     # https://stackoverflow.com/questions/898669/how-can-i-detect-if-a-file-is-binary-non-text-in-python
 
-    file_contents = ""
+    #file_contents = ""
     try:
         with open(file_path, "rt") as file_read:
-            file_contents = file_read.read()
+            template_dict['file_contents'] = file_read.read().replace('{', '{{')
     except UnicodeDecodeError:
         return "ERROR: UnicodeDecodeError - bad file"
 
-    return file_contents
+    return pystache.render(pystache_template_createfile, template_dict)
 
 def main():
     """Only called if this script is run directly"""

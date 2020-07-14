da201482404e2f6e557e86b56d5ffb681e1e707f
jgstew <james@jgstew.com>
2020-07-14 12:36:19 -0400

    everything working, attempting to detect if files already exist and not redownload them, but seems like some are being overwritten and redownloading each time. Somehow the files have the same name which shouldn't be possible

--- examples/BIOS_from_dell_catalog.py
+++ examples/BIOS_from_dell_catalog.py
@@ -1,6 +1,8 @@
 
 import xml.etree.ElementTree as ElementTree
 import datetime
+import urllib.error
+import os
 
 import sys
 # add parent directory(s) to path search for python modules
@@ -10,40 +12,63 @@ sys.path.append('../../')
 
 from bigfix_prefetch.prefetch_from_dictionary import *
 
-#import bigfix_prefetch.url_to_prefetch
+from bigfix_prefetch.url_to_prefetch import *
 
 # https://github.com/jgstew/generate_bes_from_template
 from generate_bes_from_template import generate_bes_from_template
 
 
 def main():
+    BUILD_DIRECTORY = "examples/build/"
     """Only called if this script is run directly"""
 
+    # TODO: automatically download and extract newest Dell CatalogPC.xml cab file
     xml_root = ElementTree.parse("../CatalogPC.xml")
 
     count = 0
 
     # https://stackoverflow.com/a/33280875/861745
     for elem in xml_root.findall("//SoftwareComponent/ComponentType[@value='BIOS']..."):
+        print(elem.attrib['vendorVersion'])
+        # https://stackoverflow.com/a/32621106/861745
+        print(datetime.datetime.strptime(elem.attrib['releaseDate'], '%B %d, %Y').strftime('%Y-%m-%d'))
         print(elem.attrib['size'])
         print(elem.attrib['hashMD5'])
         print("http://downloads.dell.com/" + elem.attrib['path'])
-        #print(elem.attrib['releaseDate'])
-        # https://stackoverflow.com/a/32621106/861745
-        print(datetime.datetime.strptime(elem.attrib['releaseDate'], '%B %d, %Y').strftime('%Y-%m-%d'))
 
-        #prefetch_dictionary_result = url_to_prefetch.url_to_prefetch( "http://downloads.dell.com/" + elem.attrib['path'] , True )
-        prefetch_dictionary_result = {'file_name': 'Latitude_5495_1.3.4.exe', 'file_size': 6154960, 'file_sha1': '3896f19d84c39d81af9db447043e2b048ab286f0', 'file_sha256': 'ab1ce685ba9c5162fadffca2f2e1d654f4b045334793446d6b1c91c0e62eea23', 'file_md5': '488d59fdd41345213f082bddbcad0be1', 'download_url': 'http://downloads.dell.com/FOLDER06217780M/1/Latitude_5495_1.3.4.exe'}
+        # do not download file if generated task already exists
+        file_exists = False
+        # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python
+        for file in os.listdir(BUILD_DIRECTORY):
+            if file.endswith(elem.attrib['vendorVersion'] + ".bes"):
+                # https://stackoverflow.com/questions/4940032/how-to-search-for-a-string-in-text-files
+                with open(os.path.join(BUILD_DIRECTORY, file)) as f:
+                    if elem.attrib['path'] in f.read():
+                        file_exists = True
+                        print(os.path.join(BUILD_DIRECTORY, file))
+                        break
+        # if file found, skip this iteration
+        if file_exists:
+            # TODO: consider reading the prefetch from the existing file
+            #           This would allow updating of the generated items without redownload
+            continue
+
+        # skip failed downloads
+        try:
+            prefetch_dictionary_result = url_to_prefetch( "http://downloads.dell.com/" + elem.attrib['path'] , True )
+        except urllib.error.HTTPError:
+            continue
+        #prefetch_dictionary_result = {'file_name': 'Latitude_5495_1.3.4.exe', 'file_size': 6154960, 'file_sha1': '3896f19d84c39d81af9db447043e2b048ab286f0', 'file_sha256': 'ab1ce685ba9c5162fadffca2f2e1d654f4b045334793446d6b1c91c0e62eea23', 'file_md5': '488d59fdd41345213f082bddbcad0be1', 'download_url': 'http://downloads.dell.com/FOLDER06217780M/1/Latitude_5495_1.3.4.exe'}
         #print(prefetch_dictionary_result)
 
         # Check file size matches:
         if int(prefetch_dictionary_result['file_size']) != int(elem.attrib['size']):
             print("ERROR: file size from download doesn't match catalog")
-            #break
+            break
         # Check md5 hash matches:
         if prefetch_dictionary_result['file_md5'] != elem.attrib['hashMD5']:
             print("ERROR: file md5 hash from download doesn't match catalog")
-            #break
+            break
 
         template_dict = {
             'template_file_path': 'examples/TEMPLATE_BIOS_Update.bes',
@@ -67,7 +92,7 @@ def main():
             models = model_elem.findtext("./Model/Display").split("/")
             for model in models:
                 template_dict['model'] = model_elem.findtext("./Display") + " " + model
-                with open("examples/build/BIOS_Update_" + template_dict['vendor'] + "_" + template_dict['model'] + "_" + template_dict['bios_version'] +".bes", 'w') as filetowrite:
+                with open(BUILD_DIRECTORY + "BIOS_Update_" + template_dict['vendor'] + "_" + template_dict['model'] + "_" + template_dict['bios_version'] +".bes", 'w') as filetowrite:
                     filetowrite.write(generate_bes_from_template.generate_bes_from_template(template_dict))
         count += 1
     print(count)

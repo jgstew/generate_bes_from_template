befca7a112d9e96757cd2dbc06880faa012db585
JGStew <james@jgstew.com>
2024-10-23 23:54:08 -0400

    add tag and release action

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""
+This is just a stub file
+This project uses setup.cfg and pyproject.toml
+This is required by `tag_and_release.yaml` action
+
+allows `python -m setup --version` to work
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()

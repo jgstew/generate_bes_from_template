88460e53653a47febf8f02015a69298b5801cdec
JGStew <james@jgstew.com>
2025-03-13 18:23:02 -0400

    add codespell pre-commit and fix

--- src/generate_bes_from_template/action_createfile_from_file.py
+++ src/generate_bes_from_template/action_createfile_from_file.py
@@ -29,32 +29,34 @@ copy __createfile "{{{file_path_destination}}}"
 
 
 def action_createfile_from_file(file_path, file_path_destination=None):
-    """Read text file, turn into BigFix Action CreateFile Commnad"""
+    """Read text file, turn into BigFix Action CreateFile Command"""
     template_dict = {}
     if not file_path_destination:
-        template_dict['file_path_destination'] = file_path
+        template_dict["file_path_destination"] = file_path
     else:
-        template_dict['file_path_destination'] = file_path_destination
+        template_dict["file_path_destination"] = file_path_destination
 
     # default token for end of file if not included
-    if 'token_end_of_file' not in template_dict:
-        template_dict['token_end_of_file'] = '_END_OF_FILE_'
+    if "token_end_of_file" not in template_dict:
+        template_dict["token_end_of_file"] = "_END_OF_FILE_"
 
-    if 'file_name' not in template_dict:
-        template_dict['file_name'] = get_filename_from_pathname(template_dict['file_path_destination'])  # pylint: disable=line-too-long
+    if "file_name" not in template_dict:
+        template_dict["file_name"] = get_filename_from_pathname(
+            template_dict["file_path_destination"]
+        )  # pylint: disable=line-too-long
 
     # https://stackoverflow.com/questions/898669/how-can-i-detect-if-a-file-is-binary-non-text-in-python
     try:
         with open(file_path, "rt") as file_read:
             # need to escape `{` for BigFix CreateFile command
-            template_dict['file_contents'] = file_read.read().replace('{', '{{')
+            template_dict["file_contents"] = file_read.read().replace("{", "{{")
     except UnicodeDecodeError:
         return "ERROR: UnicodeDecodeError - bad file"
 
     # make sure file contents does not contain END_OF_FILE token
-    while template_dict['token_end_of_file'] in template_dict['file_contents']:
-        template_dict['token_end_of_file'] = (
-            "_" + template_dict['token_end_of_file'] + "_"
+    while template_dict["token_end_of_file"] in template_dict["file_contents"]:
+        template_dict["token_end_of_file"] = (
+            "_" + template_dict["token_end_of_file"] + "_"
         )
 
     return chevron.render(PYSTACHE_TEMPLATE_CREATEFILE, template_dict)
@@ -63,7 +65,7 @@ def action_createfile_from_file(file_path, file_path_destination=None):
 def get_filename_from_pathname(pathname):
     """splits the filename from end of string after path separators including }"""
     # https://github.com/jgstew/tools/blob/master/Python/get_filename_from_pathname.py
-    return pathname.replace('\\', '/').replace('}', '/').split('/')[-1]
+    return pathname.replace("\\", "/").replace("}", "/").split("/")[-1]
 
 
 def main():
@@ -75,5 +77,5 @@ def main():
 
 
 # if called directly, then run this example:
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
